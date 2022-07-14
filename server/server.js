3b7b0312a055217796256af6eabf82ebe4475192
managment_system <mas152q@gmail.com>
2022-07-14 23:58:57 +1000

    routing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas/index');
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  app.listen(PORT, () => {
+    console.log(`Server is Running on ${PORT}`);
+  });
+})();

da57250aa38b73aa1fc133139970eca63ba96ca4
managment_system <mas152q@gmail.com>
2022-07-15 01:26:04 +1000

    spelling

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
   {
     useNewUrlParser: true,
-    useUnifiedTopolgy: true,
+    useUnifiedTopology: true,
   }
 );
 
